59dc400b811179fc0373bf701e167c3fb0008098
Vishal6283dk <vishal25goyal25@gmail.com>
2025-06-06 15:47:19 +0530

    Data Ingestion Component

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.DataScienceProject import logger
 
-logger.info("Welcome to my Custom Logging DataScience")
+# logger.info("Welcome to my Custom Logging DataScience")
+
+from src.DataScienceProject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/DataScienceProject/constants/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH=Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
+
+
+__all__ = ["CONFIG_FILE_PATH", "PARAMS_FILE_PATH", "SCHEMA_FILE_PATH"]
